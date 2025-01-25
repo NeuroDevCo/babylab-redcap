9dd58163bf831340ea69a3c89c294c284ca70601
gongcastro <gongarciacastro@gmail.com>
2025-01-25 07:40:18 +0100

    USe waitress for production

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 """Run app."""
 
-from .babylab.app import create_app
+from waitress import serve
+from babylab.app import create_app
 
 app = create_app(env="prod")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    # app.run(debug=False)
+    serve(app, host="127.0.0.1", port="5000")
