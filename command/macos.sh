5262bc8fb41ff147821210ef6c6f5ccccbc7beaa
Gonzalo García-Castro <gongarciacastro@gmail.com>
2025-04-04 10:35:56 +0200

    Prioritize launching app on Edge, Firefox, and Safari

--- command/macos.sh
+++ command/macos.sh
@@ -1,2 +1,15 @@
-open http://127.0.0.1:5000
-python3 -m flask --app babylab.app run
+#!/bin/bash
+URL="http://127.0.0.1:5000"
+
+if open -a "Safari" "$URL" & python3 -m flask --app babylab.app run; then
+    exit 0
+elif open -a "Microsoft Edge" "$URL" & python3 -m flask --app babylab.app run; then
+    exit 0
+elif open -a "Firefox" "$URL" & python3 -m flask --app babylab.app run; then
+    exit 0
+elif open -a "Google Chrome" "$URL" & python3 -m flask --app babylab.app run; then
+    exit 0
+else
+    echo "No supported browser found."
+    exit 1
+fi
