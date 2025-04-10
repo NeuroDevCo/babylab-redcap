0e5d56f6df1a48a1dee85cbdcf948dec708d6980
gongcastro <gongarciacastro@gmail.com>
2025-04-10 09:53:57 +0200

    Rename variables to lowercase

--- dev/dev.py
+++ dev/dev.py
@@ -8,16 +8,15 @@ from babylab.app import config as conf
 from tests import utils as tutils
 
 
-TOKEN = conf.get_api_key()
-RECORDS = api.Records(token=TOKEN)
-DATA_DICT = api.get_data_dict(token=TOKEN)
-PPT_REC = tutils.create_record_ppt()
-APT_REC = tutils.create_record_apt()
-QUE_REC = tutils.create_record_que()
-PPT_FINPUT = tutils.create_finput_ppt()
-APT_FINPUT = tutils.create_finput_apt()
-QUE_FINPUT = tutils.create_finput_que()
-APP = create_app(env="test")
-CLIENT = APP.test_client()
-APP.config["API_KEY"] = TOKEN
-APP.config["EMAIL"] = "gonzalo.garcia@sjd.es"
+token = conf.get_api_key()
+records = api.Records(token=token)
+data_dict = api.get_data_dict(token=token)
+ppt = tutils.create_record_ppt()
+apt = tutils.create_record_apt()
+que = tutils.create_record_que()
+ppt_finput = tutils.create_finput_ppt()
+apt_finput = tutils.create_finput_apt()
+que_finput = tutils.create_finput_que()
+app = create_app(env="test")
+client = app.test_client()
+app.config["API_KEY"] = token
