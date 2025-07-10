825cca233af404befdaa94003d45a250ea429897
Gonzalo García-Castro <gongarciacastro@gmail.com>
2025-07-10 16:35:24 +0200

    Use PyInstaller to make an .exe file

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 """Run app."""
 
-from waitress import serve
+# from waitress import serve
+from flaskwebgui import FlaskUI
 from babylab.app import create_app
 
 app = create_app(env_="prod")
+ui = FlaskUI(app=app, server="flask", port=5000, fullscreen=True)
 
 if __name__ == "__main__":
-    serve(app, host="127.0.0.1", port="5000")
+    # serve(app, host="127.0.0.1", port="5000")
+    ui.run()
